3254b1b3e75302f2430cee654b605219e981428d
syberflea <syberlea@gmail.com>
2025-06-30 23:37:48 +0300

    Add first model

--- viewer/models.py
+++ viewer/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
-# Create your models here.
+
+class Source(models.Model):
+    link = models.URLField(
+        max_length=200,
+        blank=True,
+        null=True
+    )
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Заголовок',
+        blank=False,
+        null=False
+    )
+    subtitle = models.CharField(
+        max_length=200,
+        verbose_name='Подзаголовок'
+    )
+    feed = models.TextField()
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации'
+    )
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Article(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Заголовок',
+        blank=False,
+        null=False
+    )
+    body = models.TextField()
+    link = models.URLField()
+    image = models.URLField()
+    guid = models.CharField(
+        max_length=200,
+        blank=True,
+        null=True
+    )
+    unread = models.BooleanField(
+        default=True,
+        null=False
+    )
+    source = models.ForeignKey(
+        Source,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='articles',
+        verbose_name='Источник',
+        help_text='Выберите источник'
+    )
+    pub_date = models.DateTimeField()
+
+    class Meta:
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.title
