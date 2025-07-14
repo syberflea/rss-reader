942167661dd4a8a48ae94969ad3c7da32d81441f
syberflea <syberlea@gmail.com>
2025-07-14 13:15:45 +0300

    Add View class

--- viewer/views.py
+++ viewer/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Article
+
+
+class HomePageView(ListView):
+    template_name = "homepage.html"
+    model = Article
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["articles"] = Article.objects.filter().order_by("-pub_date")[:10]
+        return context
