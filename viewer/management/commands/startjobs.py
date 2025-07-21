8690b380dd9dd8668835b43e19c76ab2a3d89363
syberflea <syberlea@gmail.com>
2025-07-21 15:06:24 +0300

    The canonical app is finished

--- viewer/management/commands/startjobs.py
+++ viewer/management/commands/startjobs.py
@@ -1,9 +1,19 @@
+import logging
+
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 import feedparser
 from dateutil import parser
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from django_apscheduler.jobstores import DjangoJobStore
+from django_apscheduler.models import DjangoJobExecution
 
 from viewer.models import Article
+
+
+logger = logging.getLogger(__name__)
 
 
 def save_new_articles(feed):
@@ -47,8 +57,53 @@ def fetch_4pda_articles():
     save_new_articles(_feed)
 
 
+def delete_old_job_executions(max_age=604_800):
+    """Deletes all apscheduler job execution logs older than `max_age`."""
+    DjangoJobExecution.objects.delete_old_job_executions(max_age)
+
+
 class Command(BaseCommand):
+    help = "Runs apscheduler."
+
     def handle(self, *args, **options):
-        fetch_elementy_articles()
-        fetch_nasa_articles()
-        fetch_4pda_articles()
+        scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
+        scheduler.add_jobstore(DjangoJobStore(), "default")
+
+        scheduler.add_job(
+            fetch_elementy_articles,
+            trigger="interval",
+            minutes=2,
+            id="Элементы: новости науки",
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("Added job: Elements.")
+
+        scheduler.add_job(
+            fetch_4pda_articles,
+            trigger="interval",
+            minutes=2,
+            id="RSS-лента 4PDA",
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("Added job: 4PDA.")
+
+        scheduler.add_job(
+            delete_old_job_executions,
+            trigger=CronTrigger(
+                day_of_week="mon", hour="00", minute="00"
+            ),  # Midnight on Monday, before start of the next work week.
+            id="Delete Old Job Executions",
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("Added weekly job: Delete Old Job Executions.")
+
+        try:
+            logger.info("Starting scheduler...")
+            scheduler.start()
+        except KeyboardInterrupt:
+            logger.info("Stopping scheduler...")
+            scheduler.shutdown()
+            logger.info("Scheduler shut down successfully!")
